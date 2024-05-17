3eba7c6db6dbba6a0c2052f7afbf8f522c75a403
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-17 12:31:21 +0200

    fix: solve an issue with an unchanged type into role property of the last commit

--- test/helpers/create-conversation-chunk.ts
+++ test/helpers/create-conversation-chunk.ts
@@ -3,7 +3,7 @@ import { ConversationChunkAttributes } from '../../src/interfaces';
 
 export default async (params: Partial<ConversationChunkAttributes>) =>
   ConversationChunkModel.create({
-    type: params.type,
+    role: params.role,
     value: params.value,
     conversationId: params.conversationId,
     format: params.format || constants.ConversationChunkFormat.TEXT,
